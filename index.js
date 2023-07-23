8ee97b8e539b8487b5edb12b69c03323eca5832a
0xSiddhant <contactsiddhant2155@gmail.com>
2023-07-23 14:47:50 +0530

    Nodemon and Env file added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+require("dotenv").config()
+
+const port = process.env.PORT
 const app = express()
 
 app.set("view engine", "hbs")
 
 app.use("/", require("./routes/home"))
 
-app.listen(3000,() => {
-    console.log("Port running on http://localhost:3000")
+app.listen(port,() => {
+    console.log(`Port running on http://localhost:${port}`)
 })
