d27760ef5e971381fdb14f911597a7b1fc66dad6
0xSiddhant <contactsiddhant2155@gmail.com>
2023-07-24 15:41:21 +0530

    QR code display UI implemented

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # App Drop 
+
+
+---
+# TODO
+- [ ] Direct Download link
+- [ ] IPA Save
+- [ ] Made it available for android as well
--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,3 +1,13 @@
+const qr = require('qrcode')
+
 exports.upload = (req, res) => {
-    res.status(200).send("OK")
+    qr.toDataURL("itms-services:///?action=download-manifest&url=https://dl.dropboxusercontent.com/s/jt1rpr58xlbim7h/App.plist", (err, src) => {
+        if (err) res.status(400).send("Unable to generate QR code");
+      
+        res.status(201).render("download", { 
+            src: src,
+            path: "download file"
+         });
+    });
+
 }
