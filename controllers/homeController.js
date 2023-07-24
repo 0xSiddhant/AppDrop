14edb8d5f4c2c4f4abfab0e455abf11d6499b56a
0xSiddhant <contactsiddhant2155@gmail.com>
2023-07-24 14:28:44 +0530

    Created public folder and migrated css code to seperate file

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,5 @@
 exports.home = (req, res) => {
-    res.status(200).render("index", {
+    res.status(200).render("home", {
         listingName: "Available Item List",
         items: [
             {
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const path = require('path')
 require("dotenv").config()
 
 const port = process.env.PORT
 const app = express()
 
+// Handlebars template engine
 app.set("view engine", "hbs")
+
+// Registered public folder
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 // route
 app.use("/", require("./routes/home"))
